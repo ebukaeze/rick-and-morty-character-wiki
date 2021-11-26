75e8ed0a330ec5b1d2f1be97b3432fe85b7f0c78
kingbaddo <wizzyblaq94@gmail.com>
2021-11-26 01:24:06 +0100

    fetching data from rick and morty api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+import {Card, Navbar, Filter, Pagination, Search} from './components/'
+
+
 function App() {
+  const api = `https://rickandmortyapi.com/api/character/?page=1`;
+  const [ fetchedData, setFetchedData ] = useState([]);
+
+  const {info, results } = fetchedData;
+  console.log(results);
+
+    useEffect(() => {
+       async function FetchData(){
+         const data = await fetch(api).then((res) => res.json());
+         setFetchedData(data);
+         console.log(data);
+       }
+       return FetchData();
+
+    }, [api])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center mb-3">Rick and Morty</h1>
+      <div className="row">
+        Filter component will be placed here 
+        <div className="col-lg-8 col-12">
+          <div className="row">
+            <Card  result={results}/>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
