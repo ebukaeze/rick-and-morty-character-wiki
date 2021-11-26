7b416a78e0bc3b28dfc77ee2ba449993d800e6bf
kingbaddo <wizzyblaq94@gmail.com>
2021-11-26 01:28:05 +0100

    components export file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Card } from './Card/Card';
+export { default as Filter } from './Filter/Filter';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Pagination } from './Pagination/Pagination';
+export { default as Search } from './Search/Search';
