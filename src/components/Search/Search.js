aeb305343bbd09823e753d8807fa5c62d4c06487
kingbaddo <wizzyblaq94@gmail.com>
2021-11-27 02:22:58 +0100

    search for characters by name

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import styles from './Search.module.scss';
 
-const Search = () => {
+const Search = ({setSearch, setPageNumber}) => {
+
+    const searchBtn = (e) =>{
+        e.preventDefault();
+    }
     return (
-        <div>
+        <form
+        className={`${styles.search} d-flex flex-row align-items-center justify-content-center gap-4 mb-5`}>
+            <input type="text" 
+                 placeholder="Search for Characters"
+                 className={`${styles.input}`}
+                 onChange={(e) => {setPageNumber(1); 
+                                   setSearch(e.target.value)
+                                   }}/>
+             <button onClick={(e) => searchBtn(e)} className={`${styles.btn} btn btn-primary fs-5`}>Search</button>
             
-        </div>
+        </form>
     )
 }
 
