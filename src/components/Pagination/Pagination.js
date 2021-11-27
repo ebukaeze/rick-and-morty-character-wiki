3f7b0345d7e8691d85543ba3df7a74a0be2f3aa8
kingbaddo <wizzyblaq94@gmail.com>
2021-11-27 02:19:16 +0100

    react pagination and bootstrap

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import ReactPaginate from 'react-paginate';
 
-const Pagination = () => {
+const Pagination = ({info, pageNumber, setPageNumber}) => {
+    /* const next = ()=>{
+        
+        setPageNumber( (prevState) => prevState + 1 )
+        
+    };
+    const prev = () => {
+        if(pageNumber === 1 ) return;
+        setPageNumber( (prevState) => prevState - 1 )
+    }; */
     return (
-        <div>
+        
+            <ReactPaginate 
+            className="pagination justify-content-center gap-4 my-5 text-white"
+            forcePage={pageNumber === 1? 0 : pageNumber - 1}
+            nextLabel="Next"
+            previousLabel="Prev"
+            nextClassName="btn btn-primary"
+            previousClassName="btn btn-primary"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            activeClassName="active"
+            onPageChange={(data) => {
+                setPageNumber(data.selected + 1)
+            }}
+            pageCount={info?.pages}/>
             
-        </div>
+       
     )
 }
 
