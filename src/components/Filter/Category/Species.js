58acd324fb0cfa5a2f73e301d4373bb8f08030cf
kingbaddo <wizzyblaq94@gmail.com>
2021-11-27 02:22:25 +0100

    filter categories

---
+++ src/components/Filter/Category/Species.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import FilterButtons from '../FilterButtons'
+
+const Species = () => {
+    let species = [
+        "Human",
+        "Alien",
+        "Humanoid",
+        "Poopybutthole",
+        "Mythological",
+        "Unknown",
+        "Animal",
+        "Disease",
+        "Robot",
+        "Cronenberg",
+        "Planet"
+    ]
+    return (
+        <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+       Species
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+      <div class="accordion-body d-flex flex-wrap gap-3">
+          {species.map((items, index) => (
+            <FilterButtons 
+            key={index}
+            name="species"
+            index={index}
+            items={items}
+            />
+          ))}
+         
+      </div>
+    </div>
+  </div>
+    )
+}
+
+export default Species
---
+++ src/components/Filter/Category/Status.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import FilterButtons from '../FilterButtons';
+
+const Status = ({setPageNumber, setStatus}) => {
+    let status = ["Alive", "Dead", "Unknown"]
+    return (
+        <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+       Status
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+      <div class="accordion-body d-flex flex-wrap gap-3">
+         {status.map((items, index) => (
+             <FilterButtons 
+             task={setStatus}
+             setPageNumber={setPageNumber}
+             key={index}
+             name="status"
+             index={index}
+             items={items}
+             />
+         ))}
+      </div>
+    </div>
+  </div>
+    )
+}
+
+export default Status
