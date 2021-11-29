9e244f9cfe3ba7112c5bd1b14d1fde2cf0894b49
kingbaddo <wizzyblaq94@gmail.com>
2021-11-29 09:45:16 +0100

    react routing

--- src/App.js
+++ src/App.js
@@ -1,55 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 
-import {Card, Navbar, Filter, Pagination, Search} from './components/'
+import { Navbar } from './components';
+import Episode from './components/Routes/Episode';
+import Location from './components/Routes/Location';
+import Characters from './components/Routes/Characters';
+import CardDetails from './components/Card/CardDetails';
+
+
 
 
 function App() {
-  const [ fetchedData, setFetchedData ] = useState([]);
-  const [pageNumber, setPageNumber ] = useState(1);
-  const [search, setSearch ] = useState("");
-  const [status, setStatus] = useState("");
-  const [gender, setGender ] = useState("")
 
-
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}`;
-
-  const {info, results } = fetchedData;
-  console.log(results);
-
-    useEffect(() => {
-       async function FetchData(){
-         const data = await fetch(api).then((res) => res.json());
-         setFetchedData(data);
-         console.log(data);
-       }
-       return FetchData();
-
-    }, [api])
   return (
     <div className="App">
-      <div className="container-fluid">
-      <h1 className="text-center mb-3">Rick and Morty</h1>
-      <Search  setSearch={setSearch} setPageNumber={setPageNumber}/>
-      <div className="row">
-        <Filter 
-         setGender={setGender}
-         status={status}
-         setStatus={setStatus} 
-         setPageNumber={setPageNumber}
-         />
+     
+       <Navbar />
+       <Routes>
+      <Route path="/" element={<Characters />} />
+            <Route path="/:id" element={<CardDetails />} />
 
-        <div className="col-lg-8 col-12">
-          <div className="row">
-            <Card  result={results}/>
-          </div>
-          <div className="">
-            <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
-          </div>
-          
-        </div>
-      </div>
-      </div>
+      <Route path="/episodes" element={<Episode />} />
+            <Route path="/episodes/:id" element={<CardDetails />} />
+
+      <Route path="/location" element={<Location />} />
+            <Route path="/location/:id" element={<CardDetails />} />
+    </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router } from 'react-router-dom';
 import './assets/scss/base.scss';
 import 'bootstrap/dist/js/bootstrap';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
