20f8e536cb90e7c6ba45e9bfffb983fb32030489
kingbaddo <wizzyblaq94@gmail.com>
2021-11-29 09:51:57 +0100

    rick and morty episode section

---
+++ src/components/Routes/Episode.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react';
+import { Card, Filter } from '..';
+import InputGroup from '../Filter/Category/InputGroup';
+
+const Episode = () => {
+    const [id, setId ] = useState(1);
+    const [info, setInfo] = useState([]);
+    const [results, setResults] = useState([])
+
+    const {name, air_date} = info;
+
+    console.log(results);
+
+    let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+     useEffect(() => {
+       async function FetchData(){
+           const data = await fetch(api).then((res) => res.json());
+           setInfo(data);
+
+           const a = await Promise.all(
+               data.characters.map((item) => {
+                   return fetch(item).then((res) => res.json());
+
+               })
+           )
+           setResults(a);
+       }
+       return FetchData();
+     }, [api])
+
+    return (
+        <div className="container">
+            <div className="row">
+                
+             <h1 className="text-center mb-3">Episode: {" "}
+             <span className="text-primary">
+             {name===""? "Unknown" : name}
+             </span>
+             </h1>
+             <h5 className="text-center">Air Date: {air_date === ""? "Unknown" : air_date}</h5>
+            </div>
+        
+                <div className="row">
+                    <div className="col-3">
+                        <h4 className="text-center mb-4">Pick Episodes</h4>
+                       <InputGroup setId={setId} name="Episode" total={50}/>
+                        </div>
+                    <div className="col-8 mt-4">
+                        <div className="row">
+                            <Card results={results}/>
+                        </div>
+                    </div>
+                    
+                </div>
+
+        </div>
+    )
+}
+
+export default Episode
+;
